0f0b61af7682c0bf4904ffc2ee7bb0d9f978d70e
Monica@Ideapad-tasya <monicasyatasya@gmail.com>
2026-09-13 20:07:23 +0700

    feat(admin,location,user): adding class model

---
+++ SolarWiseCore/Models/Location.cs
@@ -0,0 +1,13 @@
+namespace MySolarApp.Core.Models;
+
+public class Location
+{
+    public string Longitude { get; set; } = string.Empty;
+    public string Latitude { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+
+    public string GetCoordinates()
+    {
+        return $"{Latitude}, {Longitude}";
+    }
+}
---
+++ SolarWiseCore/Models/User.cs
@@ -0,0 +1,9 @@
+namespace MySolarApp.Core.Models;
+
+public class User
+{
+    public int UserID { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string EmailAddress { get; set; } = string.Empty;
+}
